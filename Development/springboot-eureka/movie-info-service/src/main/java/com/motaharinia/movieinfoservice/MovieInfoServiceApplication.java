7df99f4fd870f40100bc44696262481fc82c555b
Motaharinia <eng.motahari@gmail.com>
2020-03-23 02:12:43 +0430

    .

--- Development/springboot-eureka/movie-info-service/src/main/java/com/motaharinia/movieinfoservice/MovieInfoServiceApplication.java
+++ Development/springboot-eureka/movie-info-service/src/main/java/com/motaharinia/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,7 +2,12 @@ package com.motaharinia.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
