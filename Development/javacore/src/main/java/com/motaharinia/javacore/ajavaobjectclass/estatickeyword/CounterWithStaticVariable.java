79d610d2b308427b861820b67e9e878b65cc629e
Motaharinia <eng.motahari@gmail.com>
2020-04-03 11:08:43 +0430

    .

---
+++ Development/javacore/src/main/java/com/motaharinia/javacore/ajavaobjectclass/estatickeyword/CounterWithStaticVariable.java
@@ -0,0 +1,13 @@
+package com.motaharinia.javacore.ajavaobjectclass.estatickeyword;
+
+public class CounterWithStaticVariable {
+    static int count=0;//will get memory only once and retain its value
+
+    CounterWithStaticVariable(){
+        count++;//incrementing the value of static variable
+
+    }
+    public int counter(){
+        return count;
+    }
+}
--- Development/javacore/src/main/java/com/motaharinia/javacore/inheritance/aggregation/Operation.java
+++
@@ -1,7 +0,0 @@
-package com.motaharinia.javacore.inheritance.aggregation;
-
-public class Operation {
-    Integer square(Integer n){
-        return n*n;
-    }
-}
